c01054d27dcde4e84b9f5ea4bacc394c889f6fef
lucascarneiro-dev <lucas.carneiro098@hotmail.com>
2021-06-16 18:58:39 -0300

    16.06.21 update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
+
 function toggleMenu() {
     var menumob = document.getElementById("menumob");
-    if (menumob.style.display === "none") {
+    
+    if (menumob.style.display != "block") {
       menumob.style.display = "block";
     } else {
       menumob.style.display = "none";
     }
-  } 
-
-  function toggleLogo() {
-    var logomob = document.getElementById("logomob");
-    if (logomob.style.display === "block") {
-      logomob.style.display = "none";
-    } else {
-      logomob.style.display = "block";
-    }
+    
   } 
