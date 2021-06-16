dc51168eaa279c62c75c89123807bfc399e4a229
lucascarneiro-dev <lucas.carneiro098@hotmail.com>
2021-06-15 22:38:16 -0300

    fix toogle menu

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
 function toggleMenu() {
     var menumob = document.getElementById("menumob");
-    var toggleButton = false
     if (menumob.style.display === "none") {
-      var toggleButton = true
       menumob.style.display = "block";
     } else {
-      var toggleButton = true
       menumob.style.display = "none";
     }
   } 
 
   function toggleLogo() {
     var logomob = document.getElementById("logomob");
-    if (logomob.style.opacity === 1) {
-      logomob.style.opacity = 0;
+    if (logomob.style.display === "block") {
+      logomob.style.display = "none";
     } else {
-      logomob.style.opacity = 1;
+      logomob.style.display = "block";
     }
   } 
