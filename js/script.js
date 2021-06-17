de3babca058bc734e1997a5b242ec69366b682ed
lucascarneiro-dev <lucas.carneiro098@hotmail.com>
2021-06-17 10:38:46 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 function toggleMenu() {
     var menumob = document.getElementById("menumob");
-    
-    if (menumob.style.display != "block") {
+    var style = menumob.style.display;
+    if (style != "block") {
       menumob.style.display = "block";
     } else {
       menumob.style.display = "none";
