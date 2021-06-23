e7a1a3e0d81ca299e282ee1125e833618e08e862
lucascarneiro-dev <lucas.carneiro098@hotmail.com>
2021-06-23 18:05:14 -0300

    23-06-2021

--- js/script.js
+++ js/script.js
@@ -1,11 +1,45 @@
 
 function toggleMenu() {
-    var menu = document.getElementById("menu");
-    var style = menu.style.display;
-    if (style != "flex") {
-      menu.style.display = "flex";
-    } else {
-      menu.style.display = "none";
+  var menu = document.getElementById("menu");
+  var style = menu.style.display;
+  if (style != "flex") {
+    menu.style.display = "flex";
+  } else {
+    menu.style.display = "none";
+  }
+
+}
+
+
+$('.carousel').slick({
+  infinite: true,
+  dots: false,
+  arrows: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  nextArrow: '<i class="fs fa-arrow-circle-right slick-next"><img src="img/carousel/right-arrow.png" style="height: 5vh"></i>',
+  prevArrow: '<i class="fas fa-arrow-circle-left slick-prev"><img src="img/carousel/left-arrow.png" style="height: 5vh"></i>',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
     }
-    
-  } 
+  ]
+});
