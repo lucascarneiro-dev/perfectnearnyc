138bda13d5999624ef1c810adbdca430d6239b11
lucascarneiro-dev <lucas.carneiro098@hotmail.com>
2021-07-20 16:51:47 -0300

    conclusão do projeto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 document.getElementById('videobgmob').play();
 
 function toggleMenu() {
@@ -12,6 +11,18 @@ function toggleMenu() {
 
 }
 
+function displayRule(ruleid) {
+  if (window.screen.width < 800) {
+    if (document.getElementById(ruleid).style.display != "flex") {
+      document.getElementById("rulesOverlay").style.display = "flex";
+      document.getElementById(ruleid).style.display = "flex";
+    } 
+    else {
+      document.getElementById("rulesOverlay").style.display = "none";
+      document.getElementById(ruleid).style.display = "none";
+    }
+  }
+}
 
 $('.carousel').slick({
   infinite: true,
@@ -24,8 +35,7 @@ $('.carousel').slick({
   autoplaySpeed: 2000,
   nextArrow: '<i class="fs fa-arrow-circle-right slick-next"><img src="img/carousel/right-arrow.png" style="height: 5vh"></i>',
   prevArrow: '<i class="fas fa-arrow-circle-left slick-prev"><img src="img/carousel/left-arrow.png" style="height: 5vh"></i>',
-  responsive: [
-    {
+  responsive: [{
       breakpoint: 770,
       settings: {
         arrows: false,
@@ -45,44 +55,43 @@ $('.carousel').slick({
     }
   ]
 });
-x=window.screen.width
-if(x<800){
-$('.amenities').slick({
-  infinite: true,
-  dots: false,
-  arrows: false,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  adaptiveHeight: true,
-  nextArrow: '<i class="fs fa-arrow-circle-right slick-next"><img src="img/carousel/right-arrow.png" style="height: 5vh"></i>',
-  prevArrow: '<i class="fas fa-arrow-circle-left slick-prev"><img src="img/carousel/left-arrow.png" style="height: 5vh"></i>',
-  responsive: [
-    {
-      breakpoint: 770,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '20px',
-        slidesToShow: 3
+x = window.screen.width
+if (x < 800) {
+  $('.amenities').slick({
+    infinite: true,
+    dots: false,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    nextArrow: '<i class="fs fa-arrow-circle-right slick-next"><img src="img/carousel/right-arrow.png" style="height: 5vh"></i>',
+    prevArrow: '<i class="fas fa-arrow-circle-left slick-prev"><img src="img/carousel/left-arrow.png" style="height: 5vh"></i>',
+    responsive: [{
+        breakpoint: 770,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 3
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '20px',
-        slidesToShow: 3
-      }
-    }
-  ]
-});
-$('#policies').slick({
-  infinite: false,
-  dots: false,
-  arrows: false,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  adaptiveHeight: true,
+    ]
+  });
+  $('#policies').slick({
+    infinite: false,
+    dots: false,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
   });
 }
